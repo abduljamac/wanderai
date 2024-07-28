26431d6443fd3ba0dc926bba9cbb1852f253a648
abduljamac <abduljama001@gmail.com>
2024-07-28 23:55:51 +0100

    feat: create tour guide mvp

---
+++ data/cities.ts
@@ -0,0 +1,27 @@
+export default [
+  {
+    id: "london",
+    title: "London",
+    img: "/assets/london.png",
+  },
+  {
+    id: "new-york",
+    title: "New York",
+    img: "/assets/new-york.png",
+  },
+  {
+    id: "japan",
+    title: "Japan",
+    img: "/assets/japan.png",
+  },
+  {
+    id: "paris",
+    title: "Paris",
+    img: "/assets/paris.png",
+  },
+  {
+    id: "dubai",
+    title: "Dubai",
+    img: "/assets/dubai.png",
+  },
+];
